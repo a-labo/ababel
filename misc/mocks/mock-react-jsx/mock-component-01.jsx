c702ee6c4e4d0f821e09f06d2f3777fada995e74
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-18 21:45:27 +0900

    Version incremented to 1.0.11

--- misc/mocks/mock-react-jsx/mock-component-01.jsx
+++ misc/mocks/mock-react-jsx/mock-component-01.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const MyComponent01 = React.createClass({
   render () {
     return (
-      <div className='my-component-01'>This is it</div>
+      <div className='my-component-01'>This is it!</div>
     )
   }
 })
